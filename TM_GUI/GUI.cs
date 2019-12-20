5145164543cf28bd519f6fde40a7d06bae7b1bc0
Marcin Szymkowski <Marcin.Szymkowski@fabrity.pl>
2019-12-20 16:36:56 +0100

    Dodanie przewijania taśmy

--- TM_GUI/GUI.cs
+++ TM_GUI/GUI.cs
@@ -12,25 +12,37 @@ namespace TM_GUI
 {
     public partial class TM_GUI : Form
     {
+        private const int MAX_LIST_VISIBILITY = 5;
+
         private TuringMachine TM;
 
         public TM_GUI()
         {
             InitializeComponent();
 
-            TM = new TuringMachine("ababbaba");
+            //TM = new TuringMachine("ababbabaaaaaabbbabbba");
+            TM = new TuringMachine("0123456789abcdefg");
 
             refreshGui();
         }
 
         public void showTape()
         {
-            foreach(var symbol in TM.tape)
+            listViewMachineTape.Items.Clear();
+
+            //TM.head <= 5 ? 0 ; 
+            for (int i = (TM.head <= MAX_LIST_VISIBILITY ? 0 : (TM.head - MAX_LIST_VISIBILITY)); i < TM.tape.Count; i++)
             {
-                listViewMachineTape.Items.Add(symbol.ToString());
+                // na liscie miesci sie 6 itemow,  
+                listViewMachineTape.Items.Add(TM.tape[i].ToString());
+
             }
 
-            listViewMachineTape.Items[TM.head].ForeColor = Color.Red;
+            //foreach(var symbol in TM.tape)
+            //{
+            //}
+
+            //listViewMachineTape.Items[(TM.head <= MAX_LIST_VISIBILITY ? TM.head : (TM.head - MAX_LIST_VISIBILITY))].ForeColor = Color.Red;
         }
 
         public void showCurrentStateName()
